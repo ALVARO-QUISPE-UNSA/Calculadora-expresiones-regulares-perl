5e682f8a640c1ac2cc7e64f5c07ef8ffba770f29
ALVARO-QUISPE-UNSA <aquispecondo@unsa.edu.pe>
2023-11-17 15:24:10 +0000

    Soporta números negativos iniciales

--- cgi-bin/calcular.pl
+++ cgi-bin/calcular.pl
@@ -4,12 +4,7 @@ use warnings;
 
 my $exp = <STDIN>;
 chomp( $exp );
-if ($exp =~ /^([\d]+)\s*([\d]+)/) {
+if ($exp =~ /^([\-?\d]+)\s*([\d]+)/) {
   print $1 + $2;
   print "\n";
 }
-=pod
-if ($exp =~ /^( [\-+*\d]+ )\+\*( [\-+*\d]+ )$/) {
-  print "$1, $2, \n";
-}
-=cut
